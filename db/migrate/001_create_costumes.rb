2d8341cee882e2a7952d13d7fc450be3b73a3a98
Kirsten Keels <keelsk@gmail.com>
2020-03-21 01:45:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.float :price
       t.string :image_url
       t.string :size
-      t.timestamps :created_at
-      t.timestamps :updated_at
+      t.timestamps
     end
   end
 end

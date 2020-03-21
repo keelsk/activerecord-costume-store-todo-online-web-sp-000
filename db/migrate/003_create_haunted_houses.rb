58b0afb758ccf3067ee567a523d0c1d48c2a9755
Kirsten Keels <keelsk@gmail.com>
2020-03-21 01:39:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,11 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.
     end
   end
 end

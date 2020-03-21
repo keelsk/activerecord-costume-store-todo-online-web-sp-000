652d7035ab306a782e2f6a90980e03b18c1be5c9
Kirsten Keels <keelsk@gmail.com>
2020-03-21 01:52:21 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_update_costumes.rb
+++ db/migrate/004_create_update_costumes.rb
@@ -1,7 +1,7 @@
 class CreateUpdateCostumes < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :created_at, :datetime
-    add_column :users, :updated_at, :datetime
+    add_column :costumes, :created_at, :datetime
+    add_column :costumes, :updated_at, :datetime
   end
   end
 end
